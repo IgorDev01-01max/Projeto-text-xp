648e4ca6a32aac28a8c2b03bfecebf69d5fdb3b7
igor <igor.alcantarakb03@gmail.com>
2025-05-25 16:05:54 -0300

    atualização ofc

---
+++ script.js
@@ -0,0 +1,13 @@
+function enviarFormulario() {
+    const name = domcument.getElementbyid('nome').value;
+    const email = domcument.getElementbyid('email').value;
+    const tell = domcument.getElementbyid('tell').value;
+}
+
+const dadosFotmulario = {
+    nome: name,
+    email: email,
+    tell: tell
+}
+
+console.log("dadosFotmulario", dadosFotmulario); 
